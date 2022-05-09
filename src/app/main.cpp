bd331d255e51b304f8a515a401714e3de99702da
wolodyx <wolodyx@yandex.ru>
2022-05-09 20:26:59 +0300

    Подключение OpenCV

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,11 +4,15 @@
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
+
+
 class MyApp: public wxApp
 {
 public:
     virtual bool OnInit();
 };
+
+
 class MyFrame: public wxFrame
 {
 public:
@@ -19,22 +23,30 @@ private:
     void OnAbout(wxCommandEvent& event);
     wxDECLARE_EVENT_TABLE();
 };
+
+
 enum
 {
     ID_Hello = 1
 };
+
+
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
     EVT_MENU(ID_Hello,   MyFrame::OnHello)
     EVT_MENU(wxID_EXIT,  MyFrame::OnExit)
     EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
 wxEND_EVENT_TABLE()
+
 wxIMPLEMENT_APP(MyApp);
+
 bool MyApp::OnInit()
 {
     MyFrame *frame = new MyFrame( "Hello World", wxPoint(50, 50), wxSize(450, 340) );
     frame->Show( true );
     return true;
 }
+
+
 MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
         : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
@@ -52,15 +64,21 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     CreateStatusBar();
     SetStatusText( "Welcome to wxWidgets!" );
 }
+
+
 void MyFrame::OnExit(wxCommandEvent& event)
 {
     Close( true );
 }
+
+
 void MyFrame::OnAbout(wxCommandEvent& event)
 {
     wxMessageBox( "This is a wxWidgets' Hello world sample",
                   "About Hello World", wxOK | wxICON_INFORMATION );
 }
+
+
 void MyFrame::OnHello(wxCommandEvent& event)
 {
     wxLogMessage("Hello world from wxWidgets!");
